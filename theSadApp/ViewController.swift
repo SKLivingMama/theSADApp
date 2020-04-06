28f3b8f5f553f321f46a9149f3040e123512cd23
Eric Zhou <eric@gofisherman.com>
2020-04-05 21:03:18 -0700

    first commit

---
+++ theSadApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  theSadApp
+//
+//  Created by 18ericz on 4/5/20.
+//  Copyright © 2020 18ericz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
